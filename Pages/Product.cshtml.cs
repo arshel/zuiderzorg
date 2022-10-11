40fa347b08b9ed2b507349915e5c7aaca33992fd
JulianvDam <1038219@hr.nl>
2022-10-11 13:00:07 +0200

    ProductTestPagina

---
+++ Pages/Product.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zuiderzorg.Pages;
+
+public class BlikModel : PageModel
+{
+    private readonly ILogger<BlikModel> _logger;
+
+    public BlikModel(ILogger<BlikModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
