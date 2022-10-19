fbe9830f88619c7c11ef439c8756a63b0d93a879
BrianPH <brian.haubrich@gmail.com>
2022-10-19 06:28:29 +0200

    Adding Product/Category page

---
+++ Pages/Product_Select_Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zuiderzorg.Pages
+{
+    public class Product_Select_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
