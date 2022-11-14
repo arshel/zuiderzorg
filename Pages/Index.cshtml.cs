a30aa8a1939ad15ca5d9de52460a512f1d1314a7
JulianvDam <1038219@hr.nl>
2022-11-14 13:06:38 +0100

    Samenvoeg check

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,5 +15,22 @@ public class IndexModel : PageModel
     public void OnGet()
     {
 
+<<<<<<< Updated upstream
+=======
+        // var salt = Salt.Create();
+        // var hash = Hash.Create("admin", salt);
+       
+        // using (var db = new UserContext())
+        //     {
+        //         // Creating a new item and saving it to the database
+        //         var newUser = new User();
+        //         newUser.Email = "123@gmail.com";
+        //         newUser.HashSalt = salt;
+        //         newUser.HashPassword = hash;
+        //         db.Users.Add(newUser);
+        //         db.SaveChanges();
+  
+        //     }
+>>>>>>> Stashed changes
     }
 }
