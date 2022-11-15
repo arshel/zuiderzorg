6922ef64f04afdee80c9d5591fa79e1e6efa03f2
Screamified <pjfm1998@live.nl>
2022-11-15 23:31:04 +0100

    Added volume slider (TODO: attach to video)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$('.bi').parent().click(function () 
+    {
+        if($(this).children(".video").get(0).paused)
+        {        
+            $(this).children(".video").get(0).play();   
+            $(this).children(".bi").fadeOut();
+    }
+    else
+    {       
+        $(this).children(".video").get(0).pause();
+        $(this).children(".bi").fadeIn();
+    }
+});
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("volume");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
