8a41070137cf1abb2c445e0c9facdd05f5de4ddd
Screamified <pjfm1998@live.nl>
2022-11-16 11:29:11 +0100

    Volume slider functionaliteit toegevoegd (TODO: Mooier maken)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -25,4 +25,7 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
+    
+    document.getElementsByTagName('video')[0].volume = parseFloat(this.value / 100);
 }
+
