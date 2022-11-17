ae2561f3e72dacf9be397321de3cf6e549432a8b
Screamified <pjfm1998@live.nl>
2022-11-17 23:58:34 +0100

    Volume slider veranderd bijna af alleen nog bubble toevoegen. TODO: bubble toevoegen

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var oldSliderVal = -1;
+function init()
+{
+    var bubble = document.getElementById('bubble');
+    var sliderInput = document.getElementById('myRange');
+
+    sliderInput.addEventListener('mousemove', moveBubble);
+}
+
+var moveBubble = function(e)
+{
+    if(oldSliderVal !== '0' && oldSliderVal !== '100')
+    {
+        bubble.style.left = e.clientX-(bubble.offsetWidth/2)+'px';
+    }
+    var sliderVal = sliderInput.value
+    bubble.innerHTML = sliderVal;
+    oldSliderVal = sliderVal;
+}
+
 
 
 $('.bi').parent().click(function () 
@@ -20,11 +40,11 @@ $('.bi').parent().click(function ()
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("volume");
-output.innerHTML = slider.value; // Display the default slider value
+// output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    // output.innerHTML = this.value;
     
     document.getElementsByTagName('video')[0].volume = parseFloat(this.value / 100);
 }
