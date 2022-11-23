d659175cf1dc5024795b3dacc6ea719aa7a5877a
BrianPH <brian.haubrich@gmail.com>
2022-11-23 04:09:24 +0100

    Print Buttons According to Database

--- Pages/Product_Select_Page.cshtml.cs
+++ Pages/Product_Select_Page.cshtml.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using zuiderzorg.Models;
+using Npgsql;
+using System.Linq;
+using zuiderzorg.Auth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace zuiderzorg.Pages
 {
@@ -7,6 +14,23 @@ namespace zuiderzorg.Pages
     {
         public void OnGet()
         {
+            GetCategories();
+            /*using (var db = new CategoryContext())
+            {
+                // Creating a new item and saving it to the database
+                var newCatergory = new Category();
+                newCatergory.Name = "Garden";
+                db.Categories.Add(newCatergory);
+                db.SaveChanges();
+
+            }*/
+        }
+        public Category[] GetCategories()
+        {
+            using var db = new CategoryContext();
+            var CatNames = db.Categories.ToArray();
+            return CatNames;
+
         }
     }
 }
