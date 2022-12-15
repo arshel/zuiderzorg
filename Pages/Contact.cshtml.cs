facef86a5d693fa3e7d6f442a82a2d2bbd625e7e
arshel melfor <arshelmelfor@hotmail.com>
2022-12-15 14:28:18 +0100

    mailing done

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Mail;
 
 namespace zuiderzorg.Pages {
     public class ContactModel : PageModel {
@@ -15,6 +17,45 @@ namespace zuiderzorg.Pages {
            
         }
 
+        private const string To = "arshelmelfor@gmail.com";
+        private const string From = "arshelmelfor@gmail.com";
         
+        private const string GoogleAppPassword = "cuqfkkkadrhkodie";
+        
+        private const string Subject = "Zuiderzorg Contact Formulier";
+  
+        [BindProperty]
+        public MailRequest MailRequest { get;set;}
+        public IActionResult OnPost(){
+
+        string  Body = "<p> Naam: " + Request.Form["Name"] + "<br> Bericht: "+ Request.Form["Body"] + "<br> telefoon-nummer: " + Request.Form["TelNumber"] + "<br> Email: " + Request.Form["Email"] +"</p>";
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(From, GoogleAppPassword),
+                EnableSsl = true,
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(From),
+                Subject = Subject,
+                Body = Body,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(To);
+
+            smtpClient.Send(mailMessage);
+            return Page();
+        }
+
     }
 }
+
+
+public class MailRequest {
+        
+        public string? From { get; set; }
+        public string? Name { get; set; }
+         public string? Body { get; set; }
+          public string? TelNumber { get; set; }
+    }
