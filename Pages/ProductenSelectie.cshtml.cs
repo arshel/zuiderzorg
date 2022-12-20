5dd0c3739649228116ff2cbd06b26c4fd680af03
BrianPH <brian.haubrich@gmail.com>
2022-12-20 22:56:25 +0100

    Added edit and remove function. Also added default picture

--- Pages/ProductenSelectie.cshtml.cs
+++ Pages/ProductenSelectie.cshtml.cs
@@ -11,34 +11,80 @@ namespace zuiderzorg.Pages
         {
         }
 
-        [BindProperty]
-        public IFormFile FileUpload { get; set; }
-        [BindProperty]
-        public ProductRequest ProductRequest { get; set; }
+        [BindProperty] public string Option { get; set; }
+        [BindProperty] public IFormFile FileUpload { get; set; }
+        [BindProperty] public ProductRequest ProductRequest { get; set; }
+
         public IActionResult OnPost()
         {
-            using (var db = new CategoryContext())
+            switch (Request.Form["Option"])
             {
-                var filePath = Path.Combine("./wwwroot/images/", FileUpload.FileName);
-                using (var stream = System.IO.File.Create(filePath))
-                {
-                    FileUpload.CopyTo(stream);
-                }
+                case "Add":
+                    using (var db = new CategoryContext())
+                    {
+                        //Creating a new item and saving it to the database
+                        var NewProduct = new Product();
+                        NewProduct.Name = Request.Form["ProductTitle"];
+                        NewProduct.Description = Request.Form["ProductDescription"];
+                        NewProduct.PriceMin = decimal.Parse(Request.Form["ProductPriceMin"], CultureInfo.InvariantCulture.NumberFormat);
+                        NewProduct.PriceMax = decimal.Parse(Request.Form["ProductPriceMax"], CultureInfo.InvariantCulture.NumberFormat);
+                        NewProduct.ProductId = Guid.NewGuid();
+                        NewProduct.ParentCategoryId = Guid.Parse(Request.Form["CategorieID"]);
+                        if (FileUpload != null)
+                        {
+                            var filePath = Path.Combine("./wwwroot/images/", FileUpload.FileName);
+                            using (var stream = System.IO.File.Create(filePath))
+                            {
+                                FileUpload.CopyTo(stream);
+                            }
 
-                //Creating a new item and saving it to the database
-                var NewProduct = new Product();
-                NewProduct.Name = Request.Form["ProductTitle"];
-                NewProduct.Description = Request.Form["ProductDescription"];
-                NewProduct.PriceMin = decimal.Parse(Request.Form["ProductPriceMin"], CultureInfo.InvariantCulture.NumberFormat);
-                NewProduct.PriceMax = decimal.Parse(Request.Form["ProductPriceMax"], CultureInfo.InvariantCulture.NumberFormat);
-                NewProduct.ProductId = Guid.NewGuid();
-                NewProduct.ParentCategoryId = Guid.Parse(Request.Form["CategorieID"]);
-                NewProduct.Image = FileUpload.FileName;
-                db.Products.Add(NewProduct);
-                db.SaveChanges();
+                            NewProduct.Image = FileUpload.FileName;
+                        }
+                        else
+                        {
+                            NewProduct.Image = "CatagoryIcons/InfoIcon.png";
+                        }
+                        db.Products.Add(NewProduct);
+                        db.SaveChanges();
 
+
+                    }
+                    return Page();
+
+                case "Edit":
+                    using (var db = new CategoryContext())
+                    {
+                        var ProductToEdit = db.Products.Where(x => x.ProductId == Guid.Parse(Request.Cookies["ProductId"])).ToList();
+                        if (ProductToEdit.Any())
+                        {
+                            ProductToEdit[0].Name = Request.Form["ProductTitle"];
+                            ProductToEdit[0].Description = Request.Form["ProductDescription"];
+                            ProductToEdit[0].PriceMin = decimal.Parse(Request.Form["ProductPriceMin"], CultureInfo.InvariantCulture.NumberFormat);
+                            ProductToEdit[0].PriceMax = decimal.Parse(Request.Form["ProductPriceMax"], CultureInfo.InvariantCulture.NumberFormat);
+                            ProductToEdit[0].Image = FileUpload.FileName;
+                            
+                            db.SaveChanges();
+                        }
+
+                    }
+                    return Page();
+
+                case "Remove":
+                    using (var db = new CategoryContext())
+                    {
+                        var ProductToRemove = db.Products.Where(x => x.ProductId == Guid.Parse(Request.Cookies["ProductId"])).ToList();
+                        if (ProductToRemove.Any())
+                        {
+                            db.Products.Remove(ProductToRemove[0]);
+                            db.SaveChanges();
+                        }
+
+                    }
+                    return Page();
+
+                default:
+                    return Page();
             }
-            return Page();
         }
         private readonly ILogger<ProductenSelectie> _logger;
 
