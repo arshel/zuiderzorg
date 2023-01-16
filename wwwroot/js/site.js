adf2504c91cb1dedc607fd0f346370a3fb0ae412
Screamified <pjfm1998@live.nl>
2023-01-16 14:12:24 +0100

    Scalability changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -35,23 +35,6 @@ $("body").on("click", "#LoginPartial", function () {
 });
 
 
-
-// Video pause and unpause
-$('.bi').parent().click(function () 
-    {
-        if($(this).children(".video").get(0).paused)
-        {        
-            $(this).children(".video").get(0).play();   
-            $(this).children(".bi").fadeOut();
-    }
-    else
-    {       
-        $(this).children(".video").get(0).pause();
-        $(this).children(".bi").fadeIn();
-    }
-});
-
-
 // Toggle function for the popovers.
 function TogglePopovers() { $('[data-bs-toggle="popover"]').popover('toggle'); }
 
@@ -63,24 +46,6 @@ $(document).ready(function () {
 });
 
 
-// get slider
-var slider = document.getElementById("myRange");
 
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    document.getElementsByTagName('video')[0].volume = parseFloat(this.value / 100);
-};
-
-const
-    range = document.getElementById('range'),
-    rangeV = document.getElementById('rangeV'),
-    setValue = ()=>{
-        const
-            newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
-            newPosition = 10 - (newValue * 0.2);
-        rangeV.innerHTML = `<span>${range.value}</span>`;
-        rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-    };
 document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
